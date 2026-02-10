e5cb8ba883a3b4f9a11ce6e074cdc2a653b41812
Elior Yousefi <elioryous@gmail.com>
2026-02-10 23:02:19 +0200

    j

--- valentines/src/App.tsx
+++ valentines/src/App.tsx
@@ -1,20 +1,33 @@
-import school from './assets/school.jpg'
-import smoke from './assets/smoke.jpg'
-import strong from './assets/strong.jpg'
-import suit from './assets/suit.jpg'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <img src={school} className="school" alt="" />
-        <img src={smoke} className="smoke" alt="" />
-        <img src={strong} className="strong" alt="" />
-        <img src={suit} className="suit" alt="" />
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <h1>Will you be my valentines?</h1>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
